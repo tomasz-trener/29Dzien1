015895f836044e0fd68ee6e68e77344e9677dd8b
kurs <kurs@DESKTOP-284UO99>
2024-10-24 15:17:59 +0200

    q

---
+++ P13ZadanieKakulatorBMI/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13ZadanieKakulatorBMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj swoja wage w kilogramach");
+            int waga = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj swój wzrost w centymetrach");
+            int wzrost = Convert.ToInt32(Console.ReadLine());
+
+            int a = 10 / 3; // wynik: 3 
+            double b = 10 / 3.0;
+
+            //  double bmi = waga / (wzrost / 100.0 * wzrost / 100.0);
+
+            double bmi = waga / Math.Pow(wzrost / Convert.ToDouble(100), 2);
+
+            // mozna zaokraglic Math.Round 
+
+            //Console.WriteLine($"Twoje BMI to {bmi:F2}");
+            Console.WriteLine($"Twoje BMI to {bmi:000.0}");
+
+            if(bmi < 18.5)
+                Console.WriteLine("Masz niedowage");
+            else if( bmi < 25)
+                Console.WriteLine("Twoja waga jest normie");
+            else if (bmi < 30)
+                Console.WriteLine("Nadwaga");
+            else if (bmi < 35)
+                Console.WriteLine("Otyłość I stopnia");
+            else if (bmi < 40)
+                Console.WriteLine("Otyłość II stopnia (kliniczna)");
+            else 
+                Console.WriteLine("Otyłość III stopnia (ekstremalna)");
+            
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ P13ZadanieKakulatorBMI/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P13ZadanieKakulatorBMI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P13ZadanieKakulatorBMI")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b78f27a0-265b-4097-b3e8-2d840410f279")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
